2d8a222fe4ec37939678507b3fa07c0351db2fb5
Earl Ford <earl.f.student@lyonscommunityschool.org>
2017-03-28 14:20:12 +0000

    highlow

---
+++ script.js
@@ -0,0 +1,9 @@
+// JavaScript File
+(document).ready(function () {
+  // All code goes here
+  var number=5;
+if (number<10)
+   { return "you got it wrong"}
+   if(number===5)
+   {return "you got it right"}
+});
